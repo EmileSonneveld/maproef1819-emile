5a20bd13524680fa79413e582fa3af203d734ebe
Emile Sonneveld <contact@emilesonneveld.be>
2019-03-23 19:04:33 +0100

    Small refactor

--- thesisScalaProject/src/main/scala/main.scala
+++ thesisScalaProject/src/main/scala/main.scala
@@ -19,7 +19,8 @@ object main extends App {
     return tmp.substring(0, firstSlah)
   }
 
-  def getCommitHashesFromLog(logString: String): Iterator[String] = {
+  def getCommitHashesFromLog(gitTopLevel: String): Iterator[String] = {
+    val logString = execCommand("cd " + gitTopLevel + " && git log --format=\"commit %H\"")
     val re = """commit ([\w]+)""".r
     re.findAllIn(logString).map(x => x.substring(7))
   }
@@ -45,26 +46,40 @@ object main extends App {
     clearGitRepo(new File(gitTopLevel))
 
 
-    val hashes = getCommitHashesFromLog(execCommand("cd " + gitTopLevel + " && git log --format=\"commit %H\""))
+    val hashes = getCommitHashesFromLog(gitTopLevel)
     for (hash <- hashes) {
+      var commitStats: CommitStats = null
       try {
         if (!hashesFromSvg.contains(hash)) {
           execCommand("cd " + gitTopLevel + " && git clean -f") // for untracked files
           execCommand("cd " + gitTopLevel + " && git checkout .") // for modified files
           execCommand("cd " + gitTopLevel + " && git checkout " + hash)
           println(hash)
-          doStatsForProject(projectPath, projectName, hash)
+          commitStats = MeasureProject.doStatsForProject(projectPath, projectName)
         }
+        // Keep commitStats null if it was calculated before
       } catch {
         case x: Throwable => {
           println("Exception while calculating stats. Manually delete all files and start again, plz.")
-          var commitStats = new CommitStats
-          commitStats.commitHash = hash
-          logToCsv(commitStats, projectName) // Add empty line to avoid calculating this commit in the future
+          commitStats = new CommitStats // Add empty line to avoid calculating this commit in the future
           clearGitRepo(new File(gitTopLevel))
         }
       }
-      //return
+
+      if(commitStats != null)
+      {
+        commitStats.commitHash = hash
+
+        val filename = "..\\svg\\pyramid.svg"
+        var svg = scala.io.Source.fromFile(filename).getLines.mkString("\n")
+
+        svg = MeasureProject.fillInPyramidTemplate(svg, commitStats, projectName)
+        Files.write(Paths.get("..\\svg\\pyramid_" + projectName + ".svg"),
+          svg.getBytes(StandardCharsets.UTF_8))
+
+
+        logToCsv(commitStats, projectName)
+      }
     }
   }
 
@@ -90,32 +105,6 @@ object main extends App {
     execCommand("cd " + gitTopLevel + " && git checkout .")
   }
 
-  def replaceTemplatesInString(file: String, commitStats: CommitStats, projectName: String): String = {
-    var svg = file
-    val nop = commitStats.nop_set.size
-    val noc = commitStats.noc_set.size
-    val nom = commitStats.nom_set.size
-    val loc = commitStats.loc
-    val cc = commitStats.cc_set.sum
-
-    svg = svg.replaceAll("%PROJECT%", projectName)
-
-    svg = svg.replaceAll("%NOP%", nop.toString)
-    svg = svg.replaceAll("%NOC%", noc.toString)
-    svg = svg.replaceAll("%NOM%", nom.toString)
-    svg = svg.replaceAll("%LOC%", loc.toString)
-    svg = svg.replaceAll("%CYCLO%", cc.toString)
-    //svg = svg.replaceAll("%FANOUT%", commitStats.fanout.toString)
-
-    val df = new DecimalFormat(".##")
-    svg = svg.replaceAll("%NOC/NOP%", df.format(noc.toDouble / nop))
-    svg = svg.replaceAll("%NOM/NOC%", df.format(nom.toDouble / noc))
-    svg = svg.replaceAll("%LOC/NOM%", df.format(loc.toDouble / nom))
-    svg = svg.replaceAll("%CYCLO/LOC%", df.format(cc.toDouble / loc))
-    //svg = svg.replaceAll("%FANOUT/CALLS%", df.format(commitStats.fanout.toDouble / commitStats.calls))
-
-    return svg
-  }
 
   def logToCsv(commitStats: CommitStats, projectName: String) = {
     val nop = commitStats.nop_set.size
@@ -133,103 +122,6 @@ object main extends App {
     finally fw.close()
   }
 
-  def doStatsForProject(projectPath: File, projectName: String, commitHash: String) {
-
-    def recursiveListFiles(f: File): Array[File] = {
-      val these = f.listFiles()
-      these ++ these.filter(_.isDirectory).flatMap(recursiveListFiles)
-    }
-
-    val scalaFiles = recursiveListFiles(projectPath)
-
-    val commitStats = new CommitStats
-    commitStats.commitHash = commitHash
-
-    def consumeFile(exampleTree: Tree) = {
-
-      val packageCollection = exampleTree.collect {
-        case q: Pkg => q.name
-      }
-      packageCollection.foreach(x => commitStats.nop_set += x.toString)
-
-      val classCollection = exampleTree.collect {
-        case q: Defn.Class => q.name
-        case q: Defn.Object => q.name
-      }
-      classCollection.foreach(x => commitStats.noc_set += x.toString)
-
-      val applysForFanout = exampleTree.collect {
-        case q: Term.Apply => q.toString()
-      }
-      commitStats.fanout = applysForFanout.length
-
-      val functionCollection = exampleTree.collect {
-        case q: Defn.Def => q.name
-
-      }
-      functionCollection.foreach(x => commitStats.nom_set += x.toString)
-
-      commitStats.loc += exampleTree.toString.count(x => x == '\n')
-    }
-
-
-    for (f <- scalaFiles) {
-      if (f.toString.endsWith(".scala")) {
-        //println(f)
-
-        val bytes = java.nio.file.Files.readAllBytes(f.toPath)
-        val text = new String(bytes, "UTF-8")
-        val input = Input.VirtualFile(f.toString, text)
-        val exampleTree = input.parse[Source].get
-
-        consumeFile(exampleTree)
-
-        val methodMap = CfgPerMethod.compute(exampleTree)
-
-        for (pair <- methodMap) {
-          val CC = CfgPerMethod.calculateCC(pair._2)
-          println(pair._1 + ": CC= " + CC)
-          commitStats.cc_set += CC
-        }
-      }
-    }
-    /*
-        var scalaRoot = projectPath.getAbsolutePath.replace("\\", "/")
-        if (!scalaRoot.endsWith("/")) scalaRoot += "/"
-        if (scalaRoot.endsWith("src/main/"))
-          scalaRoot = scalaRoot.substring(0, scalaRoot.length - "src/main/".length)
-
-        //execCommand("cd " + getGitTopLevel(new File(scalaRoot)) + " && sbt semanticdb").trim
-
-        val semanticDB = new SemanticDB(new File(scalaRoot))
-        val documents = semanticDB.documents
-
-        for (main_doc <- documents) {
-          println("\n Doc: " + main_doc.tdoc.uri)
-          val doc = semanticDB.reload(main_doc.tdoc.uri)
-
-          consumeFile(doc.sdoc.tree)
-
-          val methodMap = CfgPerMethod.compute(doc.sdoc.tree)
-
-          for (pair <- methodMap) {
-            val CC = CfgPerMethod.calculateCC(pair._2)
-            println(pair._1 + ": CC= " + CC)
-            commitStats.cc_set += CC
-          }
-        }
-    */
-
-    val filename = "..\\svg\\pyramid.svg"
-    var svg = scala.io.Source.fromFile(filename).getLines.mkString("\n")
-
-    svg = replaceTemplatesInString(svg, commitStats, projectName)
-    Files.write(Paths.get("..\\svg\\pyramid_" + projectName + ".svg"),
-      svg.getBytes(StandardCharsets.UTF_8))
-
-
-    logToCsv(commitStats, projectName)
-  }
 
   //goTroughAllCommits(new File("C:\\Users\\emill\\dev\\scalafixtemplate\\src\\main"))
   goTroughAllCommits(new File("C:\\Users\\emill\\dev\\CTT-editor\\src\\main"))
