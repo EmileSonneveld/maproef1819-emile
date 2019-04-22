37f991df63f4fac39857b7e6d3b704f980b7b5ea
Emile Sonneveld <contact@emilesonneveld.be>
2019-04-22 19:25:05 +0200

    Kill SBT hiarchy after timeout.

--- thesisScalaProject/src/main/scala/Cmd.scala
+++ thesisScalaProject/src/main/scala/Cmd.scala
@@ -1,6 +1,9 @@
 import java.io._
 import java.nio.file._
 
+import com.sun.jna.Pointer
+import com.sun.jna.platform.win32.WinNT
+
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
@@ -33,6 +36,29 @@ object Cmd {
     new File(res)
   }
 
+  import java.lang.reflect.Field
+
+  /*
+    private def killWinProcess(process: Process) = {
+      var exitValue = 0
+      try {
+        val f = process.getClass.getDeclaredField("handle")
+        f.setAccessible(true)
+        val hndl = f.getLong(process)
+        val kernel = Kernel32.INSTANCE
+        val handle = new Kernel32 {}
+        handle.setPointer(Pointer.createConstant(hndl))
+        val pid = kernel.GetProcessId(handle)
+        killPID("" + pid)
+        exitValue = waitForProcessDeath(process, 10000)
+      } catch {
+        case ex: Throwable =>
+          log.warning("Process refused to die after 10 seconds, and couldn't killall it")
+          ex.printStackTrace()
+          throw new RuntimeException("Process refused to die after 10 seconds, and couldn't killall it: " + ex.getMessage, ex)
+      }
+      exitValue
+    }*/
   /**
     * Doesn't do cd right!
     */
@@ -48,16 +74,35 @@ object Cmd {
     val p = Process(command, cd).run(ProcessLogger(outputBuffer append _)) // start asynchronously
     val f = Future(blocking(p.exitValue())) // wrap in Future
     try {
-      val exitValue: Int = Await.result(f, duration.Duration(5 * 60, "sec"))
+      val exitValue: Int = Await.result(f, duration.Duration(1, "sec"))
       if (exitValue != 0)
         println("exitValue: " + exitValue)
     } catch {
       case _: TimeoutException =>
         outputBuffer.append(outputBuffer mkString "\n")
         println("TIMEOUT!")
-        p.destroy()
+        //p.destroy()
+        killProccesHiarchy(p)
+      //ProcessUtils.killProcess(p.)
     }
     outputBuffer.mkString("\n")
+  }
+
+  def killProccesHiarchy(process: Process): Unit = {
+    val f1 = process.getClass.getDeclaredField("p")
+    f1.setAccessible(true)
+    val pImpl = f1.get(process)
+
+    val f2 = pImpl.getClass.getDeclaredField("handle")
+    f2.setAccessible(true)
+    val hndl = f2.getLong(pImpl)
+
+    val kernel = Kernel32.INSTANCE
+    val handle = new WinNT.HANDLE
+    handle.setPointer(Pointer.createConstant(hndl))
+    val pid = kernel.GetProcessId(handle)
+
+    WindowsUtils.killPID("" + pid)
   }
 
   // In case of complicated git mistakes
