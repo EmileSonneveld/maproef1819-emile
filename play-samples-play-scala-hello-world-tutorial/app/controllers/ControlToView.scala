057cc1df67fa6b5a9ff4b8ae5da79c07514dcbc5
EmileSonneveld <contact@emilesonneveld.be>
2019-08-17 18:41:29 +0200

    Scafolding for data visualisation.

---
+++ play-samples-play-scala-hello-world-tutorial/app/controllers/ControlToView.scala
@@ -0,0 +1,10 @@
+package controllers
+
+class ControlToView {
+  var project_list: Array[String] = null
+  var project_selected: String = null
+  var commit_list: Array[String] = null
+  var commit_selected:String = null
+  var tab_list: Array[String] = null
+  var tab_selected:String = null
+}
--- play-samples-play-scala-hello-world-tutorial/app/controllers/HomeController.scala
+++ play-samples-play-scala-hello-world-tutorial/app/controllers/HomeController.scala
@@ -5,29 +5,42 @@ import play.api._
 import play.api.mvc._
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
+  def getProjects: Array[String] = {
+    Array("projectA", "projectB")
+  }
+
+  def getCommits: Array[String] = {
+    Array("86465456", "78978978654")
+  }
+  def getTabs: Array[String] = {
+    Array("Overview Pyramid", "Class diagram", "Design Smells", "CFG per Method")
+  }
+
+  var name = "Emile Sonneveld"
+
   /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    var controlToView = new ControlToView()
+    controlToView.project_list = getProjects
+    controlToView.commit_list = getCommits
+    controlToView.tab_list = getTabs
+    Ok(views.html.index(controlToView))
   }
-  
-  def explore() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.explore())
-  }
-  
+
   def tutorial() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.tutorial())
   }
-  
+
 }
